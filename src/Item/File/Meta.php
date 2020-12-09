2bec419c7292ea870eaba0ccb7e0c96a2a960d9d
Chris Cornutt <ccornutt@duosecurity.com>
2020-12-09 13:24:16 -0700

    adding additional fields to the Meta file object

--- src/Item/File/Meta.php
+++ src/Item/File/Meta.php
@@ -5,4 +5,10 @@ namespace Psecio\Statext\Item\File;
 class Meta extends \Psecio\Statext\Item
 {
     protected $title = '';
+    protected $description = '';
+    protected $icon = '';
+    protected $content_type = '';
+    protected $main = false;
+    protected $list = '';
+    protected $items = [];
 }
