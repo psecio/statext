abde9faae8e7522547848db9fe85fda6e2621893
Chris Cornutt <ccornutt@duosecurity.com>
2020-12-09 13:26:27 -0700

    massive update to the build command for meta changes, objects, looping and custom twig functions

--- src/Command/BuildCommand.php
+++ src/Command/BuildCommand.php
@@ -9,46 +9,61 @@ use Symfony\Component\Console\Input\InputOption;
 
 use Psecio\Statext\Collection;
 use Psecio\Statext\Item\File;
+use Psecio\Statext\Item\Directory;
 
 use Psecio\Statext\Item\File\Output;
 use Psecio\Statext\Item\File\Meta;
 use Psecio\Statext\Item\File\Display;
 use Psecio\Statext\Item\File\Source;
+use Psecio\Statext\Config;
 
 use League\CommonMark\CommonMarkConverter;
+use Symfony\Component\Yaml\Yaml;
+use Sabre\VObject;
 
 class BuildCommand extends Command
 {
     protected static $defaultName = 'build';
+    protected $defaultLayout = 'layout.twig';
+
+    protected $staticDirs = ['assets'];
 
     protected function configure()
     {
         $this
-            ->setDescription('Runs a test')
-            ->setHelp('This comamnd just runs a test')
+            ->setDescription('Runs the site build process')
+            ->setHelp('This command runs the full build and output process of the static files')
 
             ->addOption('source', null, InputOption::VALUE_REQUIRED, 'Source directory path')
             ->addOption('target', null, InputOption::VALUE_REQUIRED, 'Target directory path')
             ;
     }
 
+    private function loadConfig($sourcePath)
+    {
+        // Look for the "config.yml" file. If not there, don't load the config
+        $configPath = realpath($sourcePath.'/config.yml');
+        if ($configPath == false) {
+            return false;
+        }
+
+        $config = Yaml::parse(file_get_contents($configPath));
+        $this->config = new Config($config);
+        return $config;
+    }
+
     private function parseMeta(&$contents)
     {
         $meta = [];
-        preg_match('/[\-]{3,}(.*?)[\-]{3,}/s', $contents, $matches);
         
+        preg_match('/[\-]{3,}(.*?)[\-]{3,}/s', $contents, $matches);
         if (isset($matches[0])) {
             // Remove the front matter from the content
             $contents = str_replace($matches[0], '', $contents);
         }
 
         if (isset($matches[1])) {
-            $parts = explode("\n", trim($matches[1]));
-            // now split it based on the ':'
-            foreach ($parts as $pindex => $part) {
-                $split = explode(':', $part);
-                $meta[$split[0]] = trim($split[1]);
-            }
+            $meta = Yaml::parse($matches[1]);
         }
 
         return $meta;
@@ -56,60 +71,154 @@ class BuildCommand extends Command
 
     private function parseSourceContent($path, $source, $target)
     {
-        $content = new Collection();
-        $directory = new \RecursiveDirectoryIterator($path);
-        $iterator = new \RecursiveIteratorIterator($directory);
+        $new = new Collection();
 
         $targetPath = realpath(getcwd().'/'.$target);
         if ($targetPath == false) {
             throw new \Exception('Target path is invalid: '.$targetPath);
         }
 
-        foreach ($iterator as $info) {
-            if (!$info->isFile()) { continue; }
+        foreach (new \DirectoryIterator($path) as $info) {
+            if ($info->isDot()) { continue; }
 
-            // Get the contents so we can parse out the front matter
-            $meta = [];
-            $pathname = $info->getPathname();
+            if ($info->getType() == 'file') {
+                $outputType = Output::TYPE_STATIC;
+                $meta = [];
+                $pathname = $info->getPathname();
+                $contents = file_get_contents($info->getPathname());
+                $name = '';
 
-            // If the pathname doesn't end in .md, ignore it
-            $ext = substr($pathname, strrpos($pathname, '.'), strlen($pathname));
-            if (strtolower($ext) !== '.md') {
-                continue;
-            }
+                // If the pathname doesn't end in .md, let it get copied over
+                $ext = substr($pathname, strrpos($pathname, '.'), strlen($pathname));
+                if (strtolower($ext) == '.md') {
+                    $outputType = Output::TYPE_SOURCE;
+                    $meta = $this->parseMeta($contents);
 
-            $contents = file_get_contents($info->getPathname());
-            $meta = $this->parseMeta($contents);
+                    $name = str_replace('.md', '', $info->getFilename());
+                }
 
-            // File path for the rendering output
-            $outputPath = str_replace([$source, '.md'], [$target, '.html'], $info->getPathname());
+                // File path for the rendering output
+                $outputPath = str_replace([$source, '.md'], [$target, '.html'], $info->getPathname());
 
-            // To get the display path, take the target path and strip off the target directory
-            $displayPath = str_replace($targetPath.'/content', '', $outputPath);
+                // To get the display path, take the target path and strip off the target directory
+                $displayPath = str_replace($targetPath.'/content', '', $outputPath);
 
-            // Everything needs some kind of title
-            if (!isset($meta['title'])) {
-                $meta['title'] = $displayPath;
-            }
+                // Everything needs some kind of title
+                if (!isset($meta['title'])) {
+                    $meta['title'] = $displayPath;
+                }
 
-            $file = new File([
-                'source' => new Source([
+                $layout = $this->config->getOption('layout');
+                if ($layout == null) {
+                    $layout = 'layout.twig';
+                }
+
+                // Check the meta and see if there's a different layout
+                if  (isset($meta['layout'])) {
+                    $layout = $meta['layout'];
+
+                    // Make sure it ends in .twig
+                    if (substr($layout, -4) !== 'twig') {
+                        $layout = $layout.'.twig';
+                    }
+                }
+
+                $output = [
+                    'path' => $outputPath,
+                    'type' => $outputType,
+                    'layout' => $layout,
+                    'name' => $name
+                ];
+                if (isset($meta['icon'])) {
+                    $output['icon'] = $meta['icon'];
+                }
+
+                $file = new File([
+                    'source' => new Source([
+                        'path' => $info->getPathname(),
+                        'name' => $info->getFilename(),
+                        'contents' => $contents
+                    ]),
+                    'output' => new Output($output),
+                    'display' => new Display([
+                        'path' => $displayPath
+                    ]),
+                    'meta' => new Meta($meta)
+                ]);
+                $new->add($file);
+
+            } elseif ($info->getType() == 'dir') {
+                $dir = new Directory([
                     'path' => $info->getPathname(),
-                    'name' => $info->getFilename(),
-                    'contents' => $contents
-                ]),
-                'output' => new Output([
-                    'path' => $outputPath
-                ]),
-                'display' => new Display([
-                    'path' => $displayPath
-                ]),
-                'meta' => new Meta($meta)
-            ]);
-            $content->add($file);
+                    'shortPath' => $info->getBaseName(),
+                    'meta' => new Meta([
+                        'title' => $info->getBasename()
+                    ]),
+                    'output' => new Output([
+                        'path' => str_replace($source, $target, $info->getPathname()),
+                        'type' => (in_array(strtolower($info->getBasename()), $this->staticDirs)) ? Output::TYPE_STATIC : Output::TYPE_SOURCE,
+                    ])
+                ]);
+                $dir->setChildren($this->parseSourceContent($info->getPathname(), $source, $target));
+                $new->add($dir);
+            }
+        }
+        return $new;
+    }
+
+    private function renderContent($content, $twig, $converter)
+    {
+        // We have to clone content because we do iteration elsewhere
+        $content = clone $content;
+
+        foreach ($content as $index => $item) {
+            if ($item instanceof \Psecio\Statext\Item\File) {
+                if ($item->getOutputType() == Output::TYPE_SOURCE) {
+                    $markup = $converter->convertToHtml($item->getContents());
+
+                    $output = $twig->render($item->getLayout(), [
+                        'markup' => $markup,
+                        'item' => $item
+                    ]);
+                } elseif ($item->getOutputType() == Output::TYPE_STATIC) {
+                    $output = $item->getContents();
+                }
+
+                file_put_contents($item->getOutputPath(), $output);
+
+            } elseif ($item instanceof \Psecio\Statext\Collection) {
+                $this->renderContent($item, $twig, $converter);
+
+            } elseif ($item instanceof \Psecio\Statext\Item\Directory) {
+                // Make sure the path exists - if not, make it
+                if (is_dir($item->getOutputPath()) == false) {
+                    mkdir($item->getOutputPath());
+                }
+                $this->renderContent($item->getChildren(), $twig, $converter);
+
+            }
+        }
+    }
+
+    protected function parseEvents($path)
+    {
+        $events = [];
+        $path = realpath($path);
+        if ($path === false) {
+            return $events;
         }
 
-        return $content;
+        $vcalendar = VObject\Reader::read(fopen($path,'r'));
+        foreach ($vcalendar->VEVENT as $event) {
+            $events[] = [
+                'summary' => $event->SUMMARY->getValue(),
+                'description' => ($event->DESCRIPTION !== null) ? $event->DESCRIPTION->getValue() : '',
+                'startDate' => new \DateTime($event->DTSTART->getValue()),
+                'endDate'  => new \DateTime($event->DTEND->getValue())
+            ];
+        }
+
+        return $events;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -122,7 +231,7 @@ class BuildCommand extends Command
         }
         $sourcePath = realpath(getcwd().'/'.$source);
         if ($sourcePath == false) {
-            $output->writeln('<error>Source path invalid</error>');
+            $output->writeln('<error>Source path invalid</error>'); 
             return Command::FAILURE;
         }
 
@@ -132,28 +241,76 @@ class BuildCommand extends Command
             return Command::FAILURE;
         }
 
+        $this->loadConfig($sourcePath);
+
+        $events = $this->parseEvents($sourcePath.'/content/events.ics');
+        if (count($events) > 0) {
+            $output->writeln('<fg=green>'.count($events).' events found</>');
+        }
+
+        // Before we write out the content, first clear out the target directory and remake the content path
+        exec('rm -rf '.realpath(getcwd().'/'.$target.'/content'));
+        mkdir(realpath(getcwd().'/'.$target).'/content');
+
         try {
             $content = $this->parseSourceContent($sourcePath.'/content', $source, $target);
-
             $loader = new \Twig\Loader\FilesystemLoader($sourcePath.'/templates');
             $twig = new \Twig\Environment($loader);
 
-            // Clone the page content collection so any other iteration doesn't impact it
+            // Clone the page content collection and events so any other iteration doesn't impact it
             $twig->addGlobal('pages', clone $content);
+            $twig->addGlobal('events', $events);
+
+            $function = new \Twig\TwigFunction('title_format', function ($title) {
+                $title = str_replace('_', ' ', $title);
+                return ucwords($title);
+            });
+            $twig->addFunction($function);
+
+            $function = new \Twig\TwigFunction('findDir', function ($name) use ($content) {
+                $found = null;
+                $loop = function($content, $name, &$found) use (&$loop) {
+                    foreach ($content as $c) {
+                        if ($c->type == 'directory') {
+                            // Check the name
+                            if ($c->shortPath == $name) {
+                                $found = $c;
+                            } else {
+                                // Recurse through the children
+                                $loop($c, $name, $found);
+                            }
+                        }
+                    }
+                };
+                $loop($content, $name, $found);
+                return $found;
+            });
+            $twig->addFunction($function);
+
+            $function = new \Twig\TwigFunction('findFile', function ($name) use ($content) {
+                $found = null;
+                $loop = function($content, $name, &$found) use (&$loop) {
+                    foreach ($content as $c) {
+                        if ($found == null && $c->type == 'file') {
+                            if (strpos($c->getSourcePath(), $name) !== false) {
+                                $found = $c;
+                            }
+                        } elseif ($found == null && $c->type == 'directory') {
+                            $loop($c->getChildren(), $name, $found);
+                        }
+                    }
+                };
+                $loop($content, $name, $found);
+                return $found;
+            });
+            $twig->addFunction($function);
 
             // For each file, translate it using Twig and write it out to the target directory
             $converter = new CommonMarkConverter([
-                'html_input' => 'strip',
-                'allow_unsafe_links' => false,
+                'allow_unsafe_links' => false
             ]);
 
-            foreach ($content as $index => $item) {
-                $markup = $converter->convertToHtml($item->getContents());
-                $rendered = $twig->render($item->getTemplate(), [
-                    'markup' => $markup
-                ]);
-                file_put_contents($item->getOutputPath(), $rendered);
-            }
+            $this->renderContent($content, $twig, $converter);
 
             $output->writeln('<fg=green>Rendering complete</>');
 
