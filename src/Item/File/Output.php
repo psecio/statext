2ad36cabf04cc57270c21debffc0a5856ac2e7f9
Chris Cornutt <ccornutt@duosecurity.com>
2020-12-09 13:24:48 -0700

    adding constants and more properties to the Output class for files;

--- src/Item/File/Output.php
+++ src/Item/File/Output.php
@@ -4,5 +4,12 @@ namespace Psecio\Statext\Item\File;
 
 class Output extends \Psecio\Statext\Item
 {
-    protected $path = '';   
+    const TYPE_STATIC = 'static';
+    const TYPE_SOURCE = 'source';
+
+    protected $path = '';
+    protected $type = 'static';
+    protected $layout = 'layout.twig';
+    protected $icon = '/assets/img/icon/default.png';
+    protected $name = '';
 }
